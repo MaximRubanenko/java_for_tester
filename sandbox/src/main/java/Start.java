cde10c75d505f255e4e18e0a6e5b7c8fd3d36786
Max Rubanenko <maxim.rubanenko@gmail.com>
2020-10-16 23:06:10 +0300

    Форматирование

--- sandbox/src/main/java/Start.java
+++ sandbox/src/main/java/Start.java
@@ -1,6 +1,6 @@
 //
-public class Start{
-	public static void main(String[] args ){
-		System.out.println("Hello, world!");
-	}
+public class Start {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
 }

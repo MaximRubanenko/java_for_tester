6c41f4c9765eda66c02607ce549f5613ce254e45
Max Rubanenko <maxim.rubanenko@gmail.com>
2020-10-28 23:29:57 +0300

    Добавлен первый тест

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+public class Square {
+  private int a;
+
+  public Square(int a) {
+    this.a = a;
+  }
+
+  public int area(){
+    return a*a;
+  }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Start.java
@@ -0,0 +1,7 @@
+package ru.stqa.pft.sandbox;
+
+public class Start {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}

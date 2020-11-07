682692aeab11c2cf601a765a0a56ee4c602269b6
Max Rubanenko <maxim.rubanenko@gmail.com>
2020-11-07 23:24:50 +0300

    Задание №5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -5,42 +5,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupHelper {
-  public WebDriver wd;
+public class GroupHelper extends HelperBase {
 
   public GroupHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void returnToGroupPage() {
-    wd.findElement(By.linkText("group page")).click();
+    click(By.linkText("group page"));
   }
 
   public void submitGroupForm() {
-    wd.findElement(By.name("submit")).click();
+    click(By.name("submit"));
   }
 
   public void fillGroupForm(GroupData groupDAta) {
-    wd.findElement(By.name("group_name")).click();
-    wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys(groupDAta.getGroupName());
-    wd.findElement(By.name("group_header")).click();
-    wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys(groupDAta.getGroupHeader());
-    wd.findElement(By.name("group_footer")).click();
-    wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys(groupDAta.getGroupFooter());
+    type("group_name", groupDAta.getGroupName());
+    type("group_header", groupDAta.getGroupHeader());
+    type("group_footer", groupDAta.getGroupFooter());
   }
 
   public void initialGroupCreation() {
-    wd.findElement(By.name("new")).click();
+    click(By.name("new"));
   }
 
   public void deleteSelectedGroup() {
-    wd.findElement(By.xpath("(//input[@name='delete'])[1]")).click();
+    click(By.xpath("(//input[@name='delete'])[1]"));
   }
 
   public void selectGroup() {
-    wd.findElement(By.xpath("(//input[@name='selected[]'])[6]")).click();
+    click(By.xpath("(//input[@name='selected[]'])[6]"));
   }
 }
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/HelperBase.java
@@ -0,0 +1,32 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HelperBase {
+  public WebDriver wd;
+
+  public HelperBase(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+
+  protected void click(By locator) {
+    wd.findElement( locator).click();
+  }
+
+  protected void type(String locator, String text) {
+    click(By.name(locator));
+    wd.findElement(By.name(locator)).clear();
+    wd.findElement(By.name(locator)).sendKeys(text);
+  }
+  public boolean isAlertPresent(){
+    try{
+      wd.switchTo().alert();
+      return true;
+    } catch(NoAlertPresentException e) {
+      return false;
+    }
+  }
+}

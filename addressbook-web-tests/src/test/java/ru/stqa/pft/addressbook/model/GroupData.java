99f6c2bc89204756c4ef3284fd7ba273aeaab344
Max Rubanenko <maxim.rubanenko@gmail.com>
2020-11-28 16:04:27 +0300

    Сравниваем значения в списках

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,9 +1,31 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.util.Objects;
+
 public class GroupData {
   private final String groupName;
   private final String groupHeader;
   private final String groupFooter;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    GroupData groupData = (GroupData) o;
+    return Objects.equals(groupName, groupData.groupName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(groupName);
+  }
+
+  @Override
+  public String toString() {
+    return "GroupData{" +
+            "groupName='" + groupName + '\'' +
+            '}';
+  }
 
   public GroupData(String groupName, String groupHeader, String groupFooter) {
     this.groupName = groupName;

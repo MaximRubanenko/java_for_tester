a26e6f1811a9c33af8587c6a068a163f0a6c9a56
Max Rubanenko <maxim.rubanenko@gmail.com>
2020-12-04 20:32:46 +0300

    Лекция 5.2. Улучшение кода тестов:

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -8,26 +9,30 @@ import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTest extends TestBase {
-  @Test
-  public void testGroupModification() {
+  @BeforeMethod
+  public void ensurePreConditions() {
     app.getNavigationHelper().gotoGroupPage();
     if (!app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("StartGroup1", null, null));
     }
+  }
+
+  @Test
+  public void testGroupModification() {
+
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    app.getGroupHelper().selectGroup(before.size()-1);
-    app.getGroupHelper().initGroupModification();
-    GroupData group = new GroupData(before.get(before.size() - 1).getId(),"StartGroup100", "textHeader100", "textFooter100");
-    app.getGroupHelper().fillGroupForm(group);
-    app.getGroupHelper().submitGroupModification();
-    app.getGroupHelper().returnToGroupPage();
+    int index = before.size() - 1;
+    GroupData group = new GroupData(before.get(index).getId(), "StartGroup100", "textHeader100", "textFooter100");
+    app.getGroupHelper().modifyGroup(index, group);
 
     List<GroupData> after = app.getGroupHelper().getGroupList();
 
     Assert.assertEquals(after.size(), before.size());
 
-    before.remove(before.size()-1);
+    before.remove(before.size() - 1);
     before.add(group);
     Assert.assertEquals(before, after);
   }
+
+
 }
